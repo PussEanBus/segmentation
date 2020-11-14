e41be09d0ce313f83fddf3d907c5884dea304672
duchm <duchm@cystack.net>
2020-11-14 14:49:01 +0700

    fix bug

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
-from flask import Flask
-from flask_cors import CORS
-from model import SiNet
 import os
 import sys
 import warnings
-warnings.filterwarnings("ignore")
 
+# ------------------ Flask --------------------- #
+
+from flask import Flask
+from flask_cors import CORS
+
+
+# ------------------ CV --------------------- #
+
+from model import SiNet
 import numpy as np
 from keras.layers import (
-    Conv2D, BatchNormalization, Dense, 
+    Conv2D, BatchNormalization, Dense,
     ZeroPadding2D, Activation, GlobalAveragePooling2D,
     Reshape, Permute, multiply, AveragePooling2D,
     UpSampling2D, Concatenate, Add, Lambda, Multiply
@@ -26,54 +31,81 @@ from imgaug import augmenters as iaa
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, BaseLogger
 from imgaug.augmentables.segmaps import SegmentationMapsOnImage
 
+
+# ------------------ General config --------------------- #
+
+warnings.filterwarnings("ignore")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# ------------------ Flask config --------------------- #
+
 HOST = '0.0.0.0'
 PORT = 5000
 DEBUG = True
+app = Flask(__name__)
+CORS(app)
+
+# ------------------ CV config --------------------- #
+
+# Image config
 IMG_HEIGHT = 224
 IMG_WIDTH = 224
 IMG_CHANNEL = 3
 CLASSES = ["Background", "Person"]
 N_CLASSES = 2
-IMAGE_DATA_FORMAT = K.image_data_format()
+TEST_IMAGE_PATH = os.path.join(BASE_DIR, 'image_test', '1.png')
 
-app = Flask(__name__)
-CORS(app)
+# Model
+WEIGHT_FILE_PATH = os.path.join(BASE_DIR, 'weights', 'best_weights_4_all.h5')
+IMAGE_DATA_FORMAT = K.image_data_format()
 K.clear_session()
 sinet = SiNet(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL, N_CLASSES)
 model = sinet.build_decoder()
-model.load_weights("weights/best_weights_4_all.h5")
+model.load_weights(WEIGHT_FILE_PATH)
 
+
+# ------------------ API --------------------- #
+
+# Predict
 @app.route("/predict")
 def predict():
-    img_origin = cv2.imread("image_test/1.png")
+    # Get and preprocess image
+    img_origin = cv2.imread(TEST_IMAGE_PATH)
     img_resize = cv2.resize(img_origin, (224, 224))[..., ::-1]
     img = np.expand_dims(img_resize, axis=0)
+
+    # Predict
     prediction = model.predict(img)
     prediction = prediction[0]
     print(prediction)
+
+    # Mask
     mask = np.reshape(prediction, (IMG_HEIGHT, IMG_WIDTH, N_CLASSES))
     mask = np.argmax(mask, axis=-1)
     mask[mask > 0] = 255
-    
+
     new_img = np.copy(img_resize)
     non_zeros_idx = np.where(mask == 0)
-#     non_zeros_idx = np.nonzero()
+    #     non_zeros_idx = np.nonzero()
     new_img[..., 0][non_zeros_idx] = 0
     new_img[..., 1][non_zeros_idx] = 0
     new_img[..., 2][non_zeros_idx] = 0
-    
-#     print(mask)
-#     for i in range(N_CLASSES):
-#     class_idx = prediction.argmax(axis=-1)
+
+    #     print(mask)
+    #     for i in range(N_CLASSES):
+    #     class_idx = prediction.argmax(axis=-1)
     mask = cv2.merge([mask, mask, mask])
-    
+
     img_resize = cv2.resize(img_resize, (512, 512))
     new_img = cv2.resize(new_img, (512, 512))
-    
-    cv2.imshow('',new_img)
-	# return "Hello world"
+
+    # Return value
+    cv2.imshow('', new_img)
+    return 'hello'
 
 
+# ------------------ Main --------------------- #
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=DEBUG)
